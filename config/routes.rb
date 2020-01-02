29650a212bb784cf0904a5be9926d1c1ba3170e8
Letícia Fernandes <leticiafs1994@hotmail.com>
2020-01-02 18:42:07 -0300

    Set reading_goals#index as root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :reading_goals
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'reading_goals#index'
 end

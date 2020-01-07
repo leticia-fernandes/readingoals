b7f550c8e2ff6b123daeb64376fe6f6d52f47115
Letícia Fernandes <leticiafs1994@hotmail.com>
2020-01-06 21:11:26 -0300

    Add exception to pages_per_day validation

--- app/models/reading_goal.rb
+++ app/models/reading_goal.rb
@@ -2,5 +2,5 @@ class ReadingGoal < ApplicationRecord
     validates :title, presence: true
     validates :number_pages, presence: true, numericality: { only_integer: true }
     validates :deadline, presence: true
-    validates :pages_per_day, numericality: { only_integer: true }
+    validates :pages_per_day, numericality: { only_integer: true }, allow_nil: true
 end

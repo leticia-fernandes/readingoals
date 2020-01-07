004970909bf187ecc7a6fd75b95243330c6e261b
Letícia Fernandes <leticiafs1994@hotmail.com>
2020-01-06 21:08:45 -0300

    Add validates

--- app/models/reading_goal.rb
+++ app/models/reading_goal.rb
@@ -1,2 +1,6 @@
 class ReadingGoal < ApplicationRecord
+    validates :title, presence: true
+    validates :number_pages, presence: true, numericality: { only_integer: true }
+    validates :deadline, presence: true
+    validates :pages_per_day, numericality: { only_integer: true }
 end
